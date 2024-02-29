6742a9a3047f7e84e58e5e049082651333ab2dbf
akshay <baviskarakshay0702@gmail.com>
2024-02-29 18:34:28 +0530

    modified: service tests

--- TeuskoApp/src/test/java/org/telusko/course/TeluskoApp/service/CourseServiceTest.java
+++ TeuskoApp/src/test/java/org/telusko/course/TeluskoApp/service/CourseServiceTest.java
@@ -1,5 +1,7 @@
 package org.telusko.course.TeluskoApp.service;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,30 +24,29 @@ class CourseServiceTest {
     private CourseService courseService;
     @MockBean
     private CourseRepo repo;
+    Course course;
+
+    @BeforeEach
+    void setUp() {
+        course = new Course("Java", "Java Course", "3", "1000");
+        course.setId(1);
+        repo.save(course);
+    }
 
     @Test
     void getAllCoursesTest() {
-        when(repo.findAll()).thenReturn(Stream.of(
-                new Course("Java", "Java Course", "3", "1000"),
-                new Course("Spring", "Spring Course", "3", "1000")
-        ).collect(Collectors.toList()));
-
-        assertEquals(2, courseService.getAllCourses().size());
+        when(repo.findAll()).thenReturn(Stream.of(course).collect(Collectors.toList()));
+        assertEquals(1, courseService.getAllCourses().size());
     }
 
     @Test
     void getCourseByIdTest(){
-        Course course = new Course("Java", "Java Course", "3", "1000");
-        course.setId(1);
         when(repo.findById(1)).thenReturn(Optional.of(course));
         assertEquals(course, courseService.getCourseById(1));
     }
 
     @Test
     void getCourseByIdNotFoundTest(){
-        Course course = new Course("Java", "Java Course", "3", "1000");
-        course.setId(1);
-
         when(repo.findById(2)).thenThrow(new CourseNotFoundException("Course not found"));
         assertThrows(CourseNotFoundException.class, () -> courseService.getCourseById(2));
     }
@@ -53,7 +54,6 @@ class CourseServiceTest {
     @Test
     void getCourseByName(){
         String courseName = "Java";
-        Course course = new Course("Java", "Java Course", "3", "1000");
         when(repo.findByCourseName(courseName))
                 .thenReturn(Optional.of(course));
 
@@ -62,52 +62,31 @@ class CourseServiceTest {
 
     @Test
     void addCourseTest(){
-        Course course = new Course("Java", "Java Course", "3", "1000");
         when(repo.save(course)).thenReturn(course);
-
         assertEquals("Course added successfully", courseService.addCourse(course));
     }
 
     @Test
     void updateCourseTest(){
-        Course existingCourse = new Course();
-        existingCourse.setId(1);
-        existingCourse.setCourseName("Java");
-        existingCourse.setCourseDescription("Java Course");
-        existingCourse.setCourseDuration("3");
-        existingCourse.setCourseFees("1000");
-
-        when(repo.findById(1)).thenReturn(Optional.of(existingCourse));
-
-
-        assertEquals("Course updated successfully", courseService.updateCourse(existingCourse));
+        when(repo.findById(1)).thenReturn(Optional.of(course));
+        assertEquals("Course updated successfully", courseService.updateCourse(course));
     }
 
     @Test
     void updateCourseNotFoundTest(){
-        Course existingCourse = new Course();
-        existingCourse.setId(1);
-        existingCourse.setCourseName("Java");
-        existingCourse.setCourseDescription("Java Course");
-        existingCourse.setCourseDuration("3");
-        existingCourse.setCourseFees("1000");
-
         when(repo.findById(2)).thenThrow(new CourseNotFoundException("Course not found"));
-
-        assertThrows(CourseNotFoundException.class, () -> courseService.updateCourse(existingCourse));
+        assertThrows(CourseNotFoundException.class, () -> courseService.updateCourse(course));
     }
 
     @Test
     void deleteCourseByIdTest(){
-        Course existingCourse = new Course();
-        existingCourse.setId(1);
-        existingCourse.setCourseName("Java");
-        existingCourse.setCourseDescription("Java Course");
-        existingCourse.setCourseDuration("3");
-        existingCourse.setCourseFees("1000");
-
-        when(repo.findById(1)).thenReturn(Optional.of(existingCourse));
-
+        when(repo.findById(1)).thenReturn(Optional.of(course));
         assertEquals("Course deleted successfully", courseService.deleteById(1));
+    }
+
+    @AfterEach
+    void tearDown() {
+        course=null;
+        repo.deleteAll();
     }
 }
