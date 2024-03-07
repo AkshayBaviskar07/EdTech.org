033ad610b324df90a90f118bc5e7ba6938ef4674
akshay <baviskarakshay0702@gmail.com>
2024-03-07 18:41:30 +0530

    modified: using web-client

--- NajafiApp/src/main/java/com/najafi/code/NajafiApp/service/CourseService.java
+++ NajafiApp/src/main/java/com/najafi/code/NajafiApp/service/CourseService.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
@@ -19,38 +20,56 @@ public class CourseService {
     private static final String DELETE_COURSE = "http://localhost:8081/Telusko/course/delete/{id}";
 
 
+    /**
+     * Retrieves all courses from the web client.
+     * @return a list of courses
+     */
     public List<Course> getAllCourse() {
-        RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.exchange(
-                GET_ALL_COURSE,
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<Course>>() {}
-        ).getBody();
+        WebClient webClient = WebClient.create();   // Create a web client
+        return webClient.get()    // Perform a GET request
+                .uri(GET_ALL_COURSE)    // set the URI for the request
+                .retrieve()     //Retrieve the response
+                .bodyToMono(new ParameterizedTypeReference<List<Course>>() {})  // Convert the response to list of courses
+                .block();   // block and retrieve the result
     }
 
+    /**
+     * Adds a course using the given course object
+     * @param course the course to be added
+     * @return the response from the server as a string
+     */
     public String addCourse(Course course) {
-        RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.postForEntity(ADD_COURSE, course, String.class).getBody();
+        WebClient webClient = WebClient.create();
+        return webClient.post()
+                .uri(ADD_COURSE)
+                .header("Content-Type", "application/json")
+                .bodyValue(course)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
     }
 
+    /*
+    * Retrieves a course by id from web client
+    * @param id is used get course by its id
+    * @return a Course fetched using web client
+    * */
     public Course getCourseById(Integer id) {
-        RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.exchange(
-                GET_COURSE_BY_ID,
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<Course>() {},
-                id).getBody();
+       WebClient webClient = WebClient.create();
+       return webClient.get()
+               .uri(GET_COURSE_BY_ID, id)
+               .retrieve()
+               .bodyToMono(Course.class)
+               .block();
     }
+
 
     public String deleteCourse(Integer id) {
-        RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.exchange(
-                DELETE_COURSE,
-                HttpMethod.DELETE,
-                null,
-                String.class,
-                id).getBody();
+      return WebClient.create()
+               .delete()
+               .uri(DELETE_COURSE, id)
+               .retrieve()
+               .bodyToMono(String.class)
+               .block();
     }
 }
